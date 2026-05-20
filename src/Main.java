a6198e12090657b9577e2061b3fcb94000a2f2a6
L1F23BSSE0241 <L1F23BSSE0241@ucp.com>
2026-05-20 10:33:42 +0500

    Added exception handling

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,42 @@ public class Main {
         System.out.print("Enter Student Name: ");
         String name = input.nextLine();
 
-        System.out.print("Enter Marks: ");
-        int marks = input.nextInt();
+        String grade = "";
 
-        String grade;
+        try {
 
-        if(marks >= 80)
-            grade = "A";
-        else if(marks >= 70)
-            grade = "B";
-        else if(marks >= 60)
-            grade = "C";
-        else
-            grade = "Fail";
-        double percentage = (marks / 100.0) * 100;
+            System.out.print("Enter Marks: ");
+            int marks = input.nextInt();
 
-        if(marks >= 50)
-            System.out.println("Status: Pass");
-        else
-            System.out.println("Status: Fail");
+            if (marks < 0 || marks > 100)
+                throw new Exception("Invalid Marks");
 
+            if (marks >= 80)
+                grade = "A";
+            else if (marks >= 70)
+                grade = "B";
+            else if (marks >= 60)
+                grade = "C";
+            else
+                grade = "Fail";
 
-        System.out.println("Student: " + name);
-        System.out.println("Grade: " + grade);
+            double percentage = (marks / 100.0) * 100;
+
+            System.out.println("Percentage: " + percentage + "%");
+
+            if (marks >= 50)
+                System.out.println("Status: Pass");
+            else
+                System.out.println("Status: Fail");
+
+            System.out.println("Student: " + name);
+            System.out.println("Grade: " + grade);
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+
+        input.close();
     }
 }
